10267a92b7a80830dbe2bf06146fe5971107b3e5
bagnaram <mbagnara@fastmail.com>
2022-05-29 15:14:27 -0500

    yambar

---
+++ yambar/day.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+while true; do
+    scheme=$(<~/DAY)
+    if [[ "$scheme" = "0" ]]; then
+        sky=""
+        echo "test|string|$sky"
+    elif [[ "$scheme" = "1" ]]; then
+        sky=""
+        echo "test|string|$sky"
+    fi
+    echo ""
+    sleep 3
+done
